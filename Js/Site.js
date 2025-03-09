5acd025c448a3de10d92bf6a81f6487f6884ae19
dontai cantrell jr <98494230+poiut23@users.noreply.github.com>
2025-03-09 01:03:08 -0600

    Add button click event to display thank you message for user engagement

---
+++ Js/Site.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('button')
+ button.addEventListener("click", buttonClick)
+
+function buttonClick(){
+   alert("THANK YOU HELPING US SAVE THE PUPPIES!!&#9787;")
+}
+
