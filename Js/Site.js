45052041b749b23e1b139c93ec64972810da46c1
dontai cantrell jr <98494230+poiut23@users.noreply.github.com>
2025-03-09 01:35:38 -0600

    Refactor form submission alert in Site.js for improved user feedback and add id attribute to nav in SignUp.html for better accessibility

--- Js/Site.js
+++ Js/Site.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('signup')
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
         alert("SUBMISSION COMPLETE, YOU'RE A HERO AMUNG PUPPIES!!!");
     });
-});
-
