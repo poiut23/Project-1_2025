3561755d9a886e85be0ea313dec077c08dea8507
dontai cantrell jr <98494230+poiut23@users.noreply.github.com>
2025-03-18 13:53:30 -0500

    Enhance dropdown menu functionality with hover effects and refactor signup form submission alert

--- Js/Site.js
+++ Js/Site.js
@@ -1,9 +1,15 @@
-$(document).ready(() => {
+$(document).ready(function() {
     const form = $('#signup');
     form.on('submit', function (e) {
         alert("SUBMISSION COMPLETE, YOU'RE A HERO AMONG PUPPIES!!!");
     });
 
-
+    
+    $('.navItem').hover(function() {
+        $(this).children('.dropDown-Content').slideDown(200);
+    }
+    , function() {
+        $(this).children('.dropDown-Content').slideUp(200);
+    });
 
 });
