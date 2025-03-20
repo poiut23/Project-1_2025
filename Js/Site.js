64740dd4f1f43d3440a464f980d3044407e45809
dontai cantrell jr <98494230+poiut23@users.noreply.github.com>
2025-03-20 03:59:15 -0500

    Enhance dropdown menu functionality and update button styles

--- Js/Site.js
+++ Js/Site.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-    const form = $('#signup');
+   
+
+
+    const form = $('#signup'); 
     form.on('submit', function (e) {
         alert("SUBMISSION COMPLETE, YOU'RE A HERO AMONG PUPPIES!!!");
     });
 
+
+// Dropdown menu
     $(".menu-dropdown").on("click", function () {
         $(".nav-dropdown").toggle();
       })
-
+// Close the dropdown menu if the user clicks outside of it
       $(document).on("click", function (e) {
         if (!$(e.target).closest('.menu-dropdown, .nav-dropdown').length) {
             $(".nav-dropdown").hide();
